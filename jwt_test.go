c8f7eaa259463452a7526d33743f96eba8e6b1a0
Elviss Strazdins <elviss@elviss.lv>
2021-01-07 00:46:02 +0200

    Merge tests into a single function

--- jwt_test.go
+++ jwt_test.go
@@ -8,51 +8,21 @@ import (
 
 func TestNewTokenNone(t *testing.T) {
 	testCases := []struct {
-		name    string
-		payload string
-		key     string
-		result  string
+		name      string
+		payload   string
+		algorithm string
+		key       string
+		result    string
 	}{
-		{"Empty token", "{}", "test", "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.e30."},
-		{"Key value", "{\"a\":\"b\"}", "test", "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJhIjoiYiJ9."},
+		{"Empty token", "{}", jwt.AlgNone, "", "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.e30."},
+		{"Key value", "{\"a\":\"b\"}", jwt.AlgNone, "", "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJhIjoiYiJ9."},
+		{"Empty token", "{}", jwt.AlgHS256, "test", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.P4Lqll22jQQJ1eMJikvNg5HKG-cKB0hUZA9BZFIG7Jk"},
+		{"Key value", "{\"a\":\"b\"}", jwt.AlgHS256, "test", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhIjoiYiJ9.fpozJzV-Il2PfKYRsp9XYnA0MD1iLr5V_Fib0QrugT8"},
 	}
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			result, _ := jwt.NewToken([]byte(testCase.payload), jwt.AlgNone, []byte(testCase.key))
-
-			if result != testCase.result {
-				t.Errorf("Invalid result, got %s, exptected %s", result, testCase.result)
-			}
-
-			valid, _ := jwt.Validate(result, []byte(testCase.key))
-
-			if !valid {
-				t.Errorf("Validation failed")
-			}
-
-			payload, _ := jwt.GetPayload(result)
-			if payload != testCase.payload {
-				t.Errorf("Invalid payload, got %s, expected %s", payload, testCase.payload)
-			}
-		})
-	}
-}
-
-func TestNewTokenHS256(t *testing.T) {
-	testCases := []struct {
-		name    string
-		payload string
-		key     string
-		result  string
-	}{
-		{"Empty token", "{}", "test", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.P4Lqll22jQQJ1eMJikvNg5HKG-cKB0hUZA9BZFIG7Jk"},
-		{"Key value", "{\"a\":\"b\"}", "test", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhIjoiYiJ9.fpozJzV-Il2PfKYRsp9XYnA0MD1iLr5V_Fib0QrugT8"},
-	}
-
-	for _, testCase := range testCases {
-		t.Run(testCase.name, func(t *testing.T) {
-			result, _ := jwt.NewToken([]byte(testCase.payload), jwt.AlgHS256, []byte(testCase.key))
+			result, _ := jwt.NewToken([]byte(testCase.payload), testCase.algorithm, []byte(testCase.key))
 
 			if result != testCase.result {
 				t.Errorf("Invalid result, got %s, exptected %s", result, testCase.result)
